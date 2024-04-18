805ac53f6114eb3d197970f4129f81db67ccb732
Moaksh <moakshkakar@gmail.com>
2024-04-18 20:45:27 +0530

    Added image pull functionality.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from model_load.lightweight_gan.lightweight_gan import load_lightweight_model
 from IPython.display import Image
-from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from pathlib import Path
 from io import BytesIO
 import base64
 import os
@@ -32,3 +33,11 @@ def generate():
     Image(buffer, format="JPG")
     imgstr = base64.b64encode(buffer.getvalue()).decode("utf-8")
     return Response(content = imgstr, media_type = "image/jpg")
+
+
+@app.get("/get_image")
+async def get_image():
+    image_path = Path("./results/default-generated--1/0.jpg")
+    if not image_path.is_file():
+        return {"error": "Image not found on the server"}
+    return FileResponse(image_path)
